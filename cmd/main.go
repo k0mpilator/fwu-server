33ccc8d472a297a5d105a6322cae9b806fd23a01
Roman <skrilev.r@basis-plus.ru>
2019-10-24 12:40:09 +0300

    add function search new firmware and add to task

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,60 @@ import (
 	"fmt"
 	"fwu-server/internal/config"
 	"io"
+	"io/ioutil"
 	"net"
 	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v2"
 )
 
 const BUFFERSIZE = 1024
+
+func firmwareName(filename string) {
+	fwimg, err := filepath.Glob("core-image-minimal-*.ubifs")
+	if err != nil {
+		log.Error().Err(err).Msg("")
+		return
+	}
+
+	if len(fwimg) != 1 {
+		log.Error().Err(err).Msg("[  ERR ] file not found")
+		return
+	}
+
+	fmt.Println("[  OK  ] found", fwimg)
+
+	buf, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		log.Fatal().Err(err).Caller().Msg("")
+	}
+
+	c := config.Conf{}
+
+	if err = yaml.Unmarshal(buf, &c); err != nil {
+		log.Fatal().Err(err).Caller().Msg("")
+	}
+
+	c.FwName = fwimg[0]
+
+	b, err := yaml.Marshal(&c)
+	if err != nil {
+		log.Fatal().Err(err).Caller().Msg("")
+	}
+
+	f, err := os.OpenFile("config.yml", os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not open file")
+	}
+	defer f.Close()
+
+	if _, err := f.Write(b); err != nil {
+		log.Error().Err(err).Msg("could not write file")
+	}
+}
 
 func fillString(retunString string, toLength int) string {
 	for {
@@ -25,12 +71,12 @@ func fillString(retunString string, toLength int) string {
 	return retunString
 }
 
-func sendFileToClient(connection net.Conn) {
+func sendFileToClient(connection net.Conn, conf config.Conf) {
 
 	fmt.Println("[  OK  ] A client has connected!")
 
 	defer connection.Close()
-	file, err := os.Open("example.bin")
+	file, err := os.Open(conf.FwName)
 	if err != nil {
 		log.Error().Err(err).Msg("")
 		return
@@ -57,16 +103,20 @@ func sendFileToClient(connection net.Conn) {
 		}
 		connection.Write(sendBuffer)
 	}
-	fmt.Println("File has been sent, closing connection!")
+	fmt.Println("[  OK  ] File has been sent, closing connection!")
 	return
 }
 
 func main() {
 
+	// search new firmware and add to task
+	firmwareName("config.yml")
+
 	// Read config yaml
 	conf := config.NewConfig("config.yml")
 
 	fmt.Println("[  OK  ] Starting server ...")
+	fmt.Println("[  OK  ] Firmware ready:", conf.FwName)
 
 	//listen interfaces
 	server, err := net.Listen(conf.NetworkType, conf.NetworkPort)
@@ -85,6 +135,6 @@ func main() {
 		}
 
 		fmt.Println("[  OK  ] Client connected")
-		go sendFileToClient(connection)
+		go sendFileToClient(connection, conf)
 	}
 }
